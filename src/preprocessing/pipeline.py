07c5545a76c4ba91f8f621ac54d1c07b5a6ddfc8
ikotsov <i.kotsovilis@gmail.com>
2025-12-20 14:53:57 +0200

    Update preprocessing pipeline

--- src/preprocessing/pipeline.py
+++ src/preprocessing/pipeline.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from .filters import filter_high_mito_cells
+from .filters import filter_high_mito_cells, filter_high_rrna_cells, filter_high_apoptosis_cells, filter_low_magnitude_genes
 from .transforms import normalize_by_library_size, log_transform
 
 
@@ -9,8 +9,11 @@ def preprocess_sc_data(raw_data, mito_percentile=95) -> pd.DataFrame:
     """
     print(f"--- Starting Preprocessing: Input Shape {raw_data.shape} ---")
 
-    # First remove dead cells, because dead cells distort the library size normalization for the healthy cells.
-    data = filter_high_mito_cells(raw_data, percentile=mito_percentile)
+    data = filter_low_magnitude_genes(raw_data)
+    data = filter_high_apoptosis_cells(data, percentile=mito_percentile)
+    data = filter_high_rrna_cells(data, percentile=mito_percentile)
+    data = filter_high_mito_cells(data, percentile=mito_percentile)
+
     data = normalize_by_library_size(data)
     data = log_transform(data)
 
