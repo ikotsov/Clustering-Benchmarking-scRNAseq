d7a1fba1da7302debba152a772a289cb0ab4072a
ikotsov <i.kotsovilis@gmail.com>
2025-12-20 18:16:59 +0200

    Remove preprocess_sc_data from __init__ exports

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,9 +1,7 @@
-from .pipeline import preprocess_sc_data
 from .filters import filter_low_magnitude_genes, filter_high_mito_cells, filter_high_apoptosis_cells, filter_high_rrna_cells
 from .transforms import normalize_by_library_size, log_transform
 
 __all__ = [
-    "preprocess_sc_data",
     "filter_low_magnitude_genes",
     "filter_high_mito_cells",
     "filter_high_apoptosis_cells",
