74e0884da973bbac725e020a086404a495c5bffd
ikotsov <i.kotsovilis@gmail.com>
2025-12-20 18:01:18 +0200

    Refactor preprocessing pipeline

--- src/preprocessing/pipeline.py
+++ src/preprocessing/pipeline.py
@@ -3,19 +3,44 @@ from .filters import filter_high_mito_cells, filter_high_rrna_cells, filter_high
 from .transforms import normalize_by_library_size, log_transform
 
 
-def preprocess_sc_data(raw_data, mito_percentile=95) -> pd.DataFrame:
+def preprocess_data(raw_data: pd.DataFrame) -> dict:
     """
-    Runs the full preprocessing pipeline.
+    Runs the full preprocessing pipeline, including filtering and normalization.
     """
-    print(f"--- Starting Preprocessing: Input Shape {raw_data.shape} ---")
+    clean_data = filter_data(raw_data)
 
-    data = filter_low_magnitude_genes(raw_data)
-    data = filter_high_apoptosis_cells(data, percentile=mito_percentile)
-    data = filter_high_rrna_cells(data, percentile=mito_percentile)
-    data = filter_high_mito_cells(data, percentile=mito_percentile)
+    # BRANCH A: LogCPM
+    print("--- Branch A: Running LogCPM ---")
+    log_cpm_data = normalize_data_with_log_cpm(clean_data)
 
-    data = normalize_by_library_size(data)
+    # BRANCH B: NB Fitting (Pearson)
+    print("--- Branch B: Running NB Pearson Residuals ---")
+    # pearson_data = normalize_data_with_pearson(clean_data)
+
+    return {
+        "log_cpm": log_cpm_data,
+        # "pearson": pearson_data
+    }
+
+
+def normalize_data_with_log_cpm(filtered_data) -> pd.DataFrame:
+
+    data = normalize_by_library_size(filtered_data)
     data = log_transform(data)
 
-    print(f"--- Finished Preprocessing: Final Shape {data.shape} ---")
+    return data
+
+
+def filter_data(raw_data) -> pd.DataFrame:
+    """
+    Runs the full filtering pipeline.
+    """
+    print(f"--- Starting Filtering: Input Shape {raw_data.shape} ---")
+
+    data = filter_low_magnitude_genes(raw_data)
+    data = filter_high_apoptosis_cells(data)
+    data = filter_high_rrna_cells(data)
+    data = filter_high_mito_cells(data)
+
+    print(f"--- Finished Filtering: Final Shape {data.shape} ---")
     return data
