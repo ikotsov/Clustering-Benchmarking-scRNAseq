fe45f78a74022d2e8c2cd4b3a190fbb45c9fced6
ikotsov <i.kotsovilis@gmail.com>
2026-03-24 20:37:46 +0200

    Sweep benchmarks over algorithms and params

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -1,11 +1,19 @@
+from itertools import product
+
+from src.clustering.registry import AVAILABLE_ALGORITHMS, ClusteringAlgorithm
+from src.preprocessing.types import NormMethod
 from src.scripts import run_experiment
 
+ACCESSION = "E-MTAB-3321"
+ALGORITHMS: tuple[ClusteringAlgorithm, ...] = tuple(AVAILABLE_ALGORITHMS)
+NORM_METHODS: tuple[NormMethod, NormMethod] = ("log_cpm", "pearson")
+PCA_OPTIONS: tuple[bool, bool] = (True, False)
+
 if __name__ == "__main__":
-    # Experiment 1: Standard K-Means on log-CPM data (both PCA and no-PCA)
-    for with_pca in (True, False):
+    for algo_name, norm_method, with_pca in product(ALGORITHMS, NORM_METHODS, PCA_OPTIONS):
         run_experiment(
-            accession="E-MTAB-3321",
-            algo_name="kmeans",
-            norm_method="log_cpm",
+            accession=ACCESSION,
+            algo_name=algo_name,
+            norm_method=norm_method,
             with_pca=with_pca,
         )
