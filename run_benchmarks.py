57dba2ddbc3c414deaccdbc3c3fde539557b206c
ikotsov <i.kotsovilis@gmail.com>
2026-03-28 15:40:55 +0200

    Support multiple datasets in benchmark runner

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -1,18 +1,19 @@
 from itertools import product
 
 from src.clustering.registry import AVAILABLE_ALGORITHMS, ClusteringAlgorithm
+from src.constants import DATASETS
 from src.preprocessing.types import NormMethod
 from src.scripts import run_experiment
 
-ACCESSION = "E-MTAB-3321"
+
 ALGORITHMS: tuple[ClusteringAlgorithm, ...] = tuple(AVAILABLE_ALGORITHMS)
 NORM_METHODS: tuple[NormMethod, NormMethod] = ("log_cpm", "pearson")
 PCA_OPTIONS: tuple[bool, bool] = (True, False)
 
 if __name__ == "__main__":
-    for algo_name, norm_method, with_pca in product(ALGORITHMS, NORM_METHODS, PCA_OPTIONS):
+    for algo_name, dataset, norm_method, with_pca in product(ALGORITHMS, DATASETS, NORM_METHODS, PCA_OPTIONS):
         run_experiment(
-            accession=ACCESSION,
+            accession=dataset,
             algo_name=algo_name,
             norm_method=norm_method,
             with_pca=with_pca,
