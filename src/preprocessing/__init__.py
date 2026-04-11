7ab289177de0a5efc9be3eb911702ee048197c04
ikotsov <i.kotsovilis@gmail.com>
2026-04-11 12:51:02 +0300

    Rename dimensionality.py to apply_pca.py

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,7 +1,7 @@
 from .preprocessing import preprocess_data
 from .filters import filter_low_magnitude_genes, filter_high_mito_cells, filter_high_apoptosis_cells, filter_high_rrna_cells, filter_doublets
 from .transforms import normalize_by_library_size, log_transform, normalize_data_with_pearson
-from .dimensionality import apply_pca
+from .apply_pca import apply_pca
 
 __all__ = [
     "filter_low_magnitude_genes",
