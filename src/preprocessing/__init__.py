34894be73694b0ac8d29033e32430034ca423d30
ikotsov <i.kotsovilis@gmail.com>
2026-09-21 20:28:36 +0300

    Rename preprocessing module to pipeline

--- src/preprocessing/__init__.py
+++ src/preprocessing/__init__.py
@@ -1,4 +1,4 @@
-from .preprocessing import preprocess_data
+from .pipeline import preprocess_data
 from .filters import filter_low_magnitude_genes, filter_high_mito_cells, filter_high_apoptosis_cells, filter_high_rrna_cells, filter_doublets
 from .transforms import normalize_by_library_size, log_transform, normalize_with_pearson
 from .apply_pca import apply_pca
